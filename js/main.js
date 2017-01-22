1244507698cfa73c3214d6f1dfc29b54dc75ca36
lfbusta <lfbusta@gmail.com>
2017-01-23 00:55:18 +0100

    Screenshot Showcase
    + Added buttons to navigate through all 5 screenshots of the game.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,90 @@
-/* ====================================================================================== */
-/*                                           MAIN                                         */
-/* ====================================================================================== */
+// ======================================================================================
+//                                       TEST
+// ======================================================================================
+
+function test(){ console.log("Test Function"); }
+
+// ======================================================================================
+//                                      FUNCTIONS
+// ======================================================================================
+let screenshotNumber = 2;
+
+function listener_nextScreenshot(){
+    screenshotNumber++;
+    if (screenshotNumber > 4){ screenshotNumber = 0; }
+    specScreenshot(screenshotNumber);
+}
+function listener_prevScreenshot(){
+    screenshotNumber--;
+    if (screenshotNumber < 0){ screenshotNumber = 4; }
+    specScreenshot(screenshotNumber);
+}
+function listener_specScreenshot(element){
+    if(element.target !== element.currentTarget){
+        let clickedItem = element.target.id;
+        screenshotNumber = parseInt(clickedItem.substring(5));
+        specScreenshot(screenshotNumber);
+    }
+    element.stopPropagation();
+}
+function specScreenshot(num){
+    changeScreenshot(num);
+    changeBg(num);
+}
+function changeScreenshot(num){
+    screenshots = document.getElementsByClassName("screenshot");
+    for(let i=0; i<screenshots.length; i++){ screenshots[i].style.display = "none"; }
+
+    element = document.getElementById(`ss0${num}`);
+    element.style.display = "block";
+}
+function changeBg(num){
+    let elements = document.getElementsByClassName("ssDot");
+
+    for (let i=0; i < elements.length; i++){
+        let element = elements[i];
+        element.style.backgroundColor = "transparent";
+    }
+
+    element = document.getElementById(`ssDot0${num}`);
+    element.style.backgroundColor = "white";
+}
+
+// ======================================================================================
+//                                      LISTENERS
+// ======================================================================================
+
+let navButtonR = document.getElementsByClassName("navButtonR")[0];
+navButtonR.addEventListener("click", listener_nextScreenshot, false);
+
+let navButtonL = document.getElementsByClassName("navButtonL")[0];
+navButtonL.addEventListener("click", listener_prevScreenshot, false);
+
+let navContainer2 = document.getElementsByClassName("navContainer2")[0];
+navContainer2.addEventListener("click", listener_specScreenshot, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function placeRandomCloud(tagName){
    target = document.getElementsByTagName(tagName);
@@ -63,10 +147,10 @@ function goldenCloudRate(tagName,className,limit){
    //console.log("Cloud Count: " + count)
 }
 
-/* ====================================================================================== */
-/*                                         ONLOAD                                         */
-/* ====================================================================================== */
+// ======================================================================================
+//                                      ONLOAD
+// ======================================================================================
 
 window.onload = function(){
-   
+
 }
